c73d94966e2567b5108faf7426c6d4bfaeba0bbd
jismiya <jismiyaprasadp01@gmail.com>
2026-02-20 15:12:19 +0530

    sll

--- 11_SLL/10_Sorting.c
+++ 11_SLL/10_Sorting.c
@@ -11,4 +11,106 @@ Sorted list: 1 2 3 5 7
 
 
 */
+#include <stdio.h>
+#include <stdlib.h>
 
+struct node {
+    int data;
+    struct node* next;
+};
+
+struct node* addend(struct node* head, int data) {
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = NULL;
+    if (head == NULL)
+        return temp;
+    struct node* curr = head;
+    while (curr->next != NULL)
+        curr = curr->next;
+    curr->next = temp;
+    return head;
+}
+
+void printlist(struct node* head) {
+    struct node* curr = head;
+    while (curr != NULL) {
+        printf("%d ", curr->data);
+        curr = curr->next;
+    }
+    printf("\n");
+}
+
+void split(struct node* source, struct node** front, struct node** back) {
+    struct node* slow = source;
+    struct node* fast = source->next;
+
+    while (fast != NULL) {
+        fast = fast->next;
+        if (fast != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+
+    *front = source;
+    *back = slow->next;
+    slow->next = NULL;
+}
+
+struct node* merge(struct node* a, struct node* b) {
+    struct node* result = NULL;
+
+    if (a == NULL)
+        return b;
+    if (b == NULL)
+        return a;
+
+    if (a->data <= b->data) {
+        result = a;
+        result->next = merge(a->next, b);
+    } else {
+        result = b;
+        result->next = merge(a, b->next);
+    }
+    return result;
+}
+
+struct node* merge_sort(struct node* head) {
+    if (head == NULL || head->next == NULL)
+        return head;
+
+    struct node* front;
+    struct node* back;
+
+    split(head, &front, &back);
+
+    front = merge_sort(front);
+    back = merge_sort(back);
+
+    return merge(front, back);
+}
+int main() 
+{
+    struct node* head = NULL;
+    int n, data;
+
+    printf("enter number of nodes\n");
+    scanf("%d", &n);
+
+    printf("enter data for nodes\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &data);
+        head = addend(head, data);
+    }
+
+    printf("original list:\n");
+    printlist(head);
+
+    head = merge_sort(head);
+
+    printf("sorted list:\n");
+    printlist(head);
+
+    return 0;
+}
--- 11_SLL/12_Merging.c
+++ 11_SLL/12_Merging.c
@@ -11,4 +11,85 @@ List-3: 10 70 100
 Expected output:
 Merged list: 10 10 20 30 40 50 60 70 100
 */
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* newNode(int data) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->data = data;
+    temp->next = NULL;
+    return temp;
+}
+
+void printList(Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+Node* mergeTwoLists(Node* l1, Node* l2) {
+    Node dummy;
+    Node* tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->data < l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } 
+        else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    tail->next = (l1 != NULL) ? l1 : l2;
+    return dummy.next;
+}
+
+Node* mergeKLists(Node* lists[], int k) {
+    if (k == 0) 
+    return NULL;
+
+    while (k > 1) {
+        int i = 0;
+        for (; i < k / 2; i++) {
+            lists[i] = mergeTwoLists(lists[i], lists[k - 1 - i]);
+        }
+        k = (k + 1) / 2;
+    }
+
+    return lists[0];
+}
+
+int main() 
+{
+    Node* list1 = newNode(10);
+    list1->next = newNode(20);
+    list1->next->next = newNode(50);
+
+    Node* list2 = newNode(30);
+    list2->next = newNode(40);
+    list2->next->next = newNode(60);
+
+    Node* list3 = newNode(10);
+    list3->next = newNode(70);
+    list3->next->next = newNode(100);
+
+    Node* lists[3] = {list1, list2, list3};
+
+    Node* mergedList = mergeKLists(lists, 3);
+
+    printf("Merged list: ");
+    printList(mergedList);
+
+    return 0;
+}
